a6225a76b175d595dad0fc2a596e261a8f189d06
fand <fand@gmork.in>
2015-02-08 17:15:30 +0900

    use karma as test runner

---
+++ test/test.js
@@ -0,0 +1,119 @@
+describe("WorkerTimer", function() {
+  var timerId;
+
+  afterEach(function() {
+    WorkerTimer.clearInterval(timerId);
+    WorkerTimer.clearTimeout(timerId);
+  });
+
+  it("setInterval(callback: function, delay: number): number", function(done) {
+    var passed = 0;
+
+    timerId = WorkerTimer.setInterval(function() {
+      passed += 1;
+    }, 25);
+
+    assert(typeof timerId === "number");
+
+    setTimeout(function() {
+      assert(passed >= 15);
+      done();
+    }, 500);
+  });
+  it("clearInterval(timerId: number): number", function(done) {
+    var passed = 0,
+      savedPassed = 0;
+
+    timerId = WorkerTimer.setInterval(function() {
+      passed += 1;
+    }, 25);
+
+    assert(typeof timerId === "number");
+
+    setTimeout(function() {
+      WorkerTimer.clearInterval(timerId);
+
+      savedPassed = passed;
+
+      setTimeout(function() {
+        assert(passed === savedPassed);
+        done();
+      }, 250);
+    }, 250);
+  });
+  it("setTimeout(callback: function, delay: number): number", function(done) {
+    var passed = 0;
+
+    timerId = WorkerTimer.setTimeout(function() {
+      passed += 1;
+    }, 25);
+
+    assert(typeof timerId === "number");
+
+    setTimeout(function() {
+      assert(passed === 1);
+      done();
+    }, 500);
+  });
+  it("clearTimeout(timerId: number): void", function(done) {
+    var passed = 0;
+
+    timerId = WorkerTimer.setTimeout(function() {
+      passed += 1;
+    }, 25);
+
+    WorkerTimer.clearTimeout(timerId);
+
+    setTimeout(function() {
+      assert(passed === 0);
+      done();
+    }, 500);
+  });
+
+  describe("setTimeout(callback: function, delay: number, timerId: number): number", function() {
+    // var timerId;
+    // afterEach(function() {
+    //   WorkerTimer.clearInterval(timerId);
+    //   WorkerTimer.clearTimeout(timerId);
+    // });
+
+    it("creates new timer when timerId is invalid", function(done) {
+      var passed = 0;
+
+      timerId = WorkerTimer.setTimeout(function() {
+        passed += 1;
+      }, 10, null);
+
+      assert(timerId !== null);
+      assert(typeof timerId === "number");
+
+      setTimeout(function() {
+        assert(passed === 1);
+        done();
+      }, 300);
+    });
+    it("recycles old timer when the id is given", function(done) {
+      var passed = 0;
+
+      var oldTimerId = WorkerTimer.setTimeout(function() {
+        passed += 1;
+      }, 10);
+
+      setTimeout(function() {
+        var newTimerId = WorkerTimer.setTimeout(function() {
+          passed += 1;
+        }, 10, oldTimerId);
+
+        var newTimerId2 = WorkerTimer.setTimeout(function() {
+          passed += 1;
+        }, 10, newTimerId);
+
+        setTimeout(function() {
+          //window.assert(passed === 2);
+          //window.assert(newTimerId === timerId);
+          done();
+        }, 100);
+      }, 300);
+    });
+  });
+});
